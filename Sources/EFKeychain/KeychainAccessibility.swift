20e2f26d42e4bf3dba9a187a2ae8d5e476681766
Justin <justin@digitalawesomeapps.com>
2022-04-14 12:11:57 -0700

    KeychainAccessibility

--- Sources/EFKeychain/KeychainAccessibility.swift
+++ Sources/EFKeychain/KeychainAccessibility.swift
@@ -8,6 +8,38 @@
 
 import Foundation
 
+protocol KeychainAttributeRepresentable {
+	var attributeValue: CFString { get }
+}
+
 public enum KeychainAccessibility {
+	/// Data in Keychain can be accessed regardless of whether the device is unlocked
+	case always
+	/// Data in Keychain cannot be accessed after a restart until the device has been unlocked by the user
+	case afterFirstUnlock
+	/// Data in Keychain cannot be accessed after a restart until the device has been unlocked by the user
+	case whenUnlocked
 	
+	static func accessibilityForValue(_ attribute: CFString) -> KeychainAccessibility? {
+		for (key, value) in accessibilityLookup {
+			if value == attribute { return key }
+		}
+		
+		return nil
+	}
+}
+
+private let accessibilityLookup: [KeychainAccessibility: CFString] = {
+	let lookup: [KeychainAccessibility: CFString] =
+		[
+		.always: 			kSecAttrAccessibleAlways,
+		.afterFirstUnlock: kSecAttrAccessibleAfterFirstUnlock,
+		.whenUnlocked: 		kSecAttrAccessibleWhenUnlocked
+		]
+	
+	return lookup
+}()
+
+extension KeychainAccessibility: KeychainAttributeRepresentable {
+	internal var attributeValue: CFString { accessibilityLookup[self]! }
 }
